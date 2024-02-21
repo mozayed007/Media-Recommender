2451005ae37de2b291d455130183ac02332d92a6
Mohamed Zayed Ahmed <mohamedzayedpentest007@gmail.com>
2024-02-22 01:43:36 +0200

    removed unwanted files

---
+++ src/anime_scraper.py
@@ -0,0 +1,9 @@
+import os
+import datetime
+import tqdm
+import time
+import math
+import json
+from client import get_data
+from common_utils import keys, anime_keys, manga_keys, known_fails, base_directory, merge_anime
+
--- src/scrape_data.py
+++
@@ -1,4 +0,0 @@
-from scraper import scrape_ranking
-
-scrape_ranking('anime', 'favorite')
-scrape_ranking('manga', 'favorite')
